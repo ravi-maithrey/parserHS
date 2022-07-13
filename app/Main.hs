30609583423af615bc01b6c5f1f5b4bcb3e9113c
ravi maithrey <maitrey.ind@gmail.com>
2022-07-13 18:47:42 +0530

    basic parser to recog symbols

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
+-- importing the parser library except spaces
 import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+-- defining a paprser which recognizes the symbols
+symbol :: Parser Char -- takes the type Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~" -- oneOf is a parser lib func to check if there is one of the defined chars
 
 
 main :: IO ()
