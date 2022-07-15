3d95e80354f28beaeb1964afed26d7c600a96a1c
ravi maithrey <maitrey.ind@gmail.com>
2022-07-15 10:58:18 +0530

    old version of the exercise

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,12 @@ import System.Environment
 symbol :: Parser Char -- takes the type Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~" -- oneOf is a parser lib func to check if there is one of the defined chars
 
-
+-- defining a func to call parser and handle errors
+readExpr :: String -> String -- func signature
+-- func definition
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right err -> "Found value"
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
